7b8a4bb1ef3ede6c10bd1e7124ce7ac14e263196
balamurugan.vg <balamurugan.vg@zohocorp.com>
2021-07-24 17:59:08 +0530

    fix

--- mailnotify2/settings.py
+++ mailnotify2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = encrypt.getSecretKey()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['evening-peak-06941.herokuapp.com', 'http://127.0.0.1']
+ALLOWED_HOSTS = ['notifyreminder.herokuapp.com', 'http://127.0.0.1']
 
 # Application definition
 
